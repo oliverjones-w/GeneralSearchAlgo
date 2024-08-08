fbe50dd39d311cb038e68241f78a76ea4a9054be
Brielle <briellesantoro@gmail.com>
2024-08-08 11:34:53 -0400

    first attempt

--- src/Master 2 PPL MVS ID ALGO.py
+++ src/Master 2 PPL MVS ID ALGO.py
@@ -4,11 +4,57 @@ from fuzzywuzzy import fuzz
 from openpyxl import Workbook
 
 # Load the data frames
-master_file_path = r"C:\Users\BSA-OliverJ'22\OneDrive\Desktop\OneDrive\Programming\Projects\GeneralizedSearchAlgo\Workbooks\HFM Master Data Frame for matching.xlsx"
-people_moves_file_path = r"C:\Users\BSA-OliverJ'22\OneDrive\Desktop\OneDrive\Programming\Projects\GeneralizedSearchAlgo\Workbooks\People Moves Data Frame.xlsx"
+master_file_path = r"C:\Users\Bay Street - Larry B\Documents\Brielle\Programming\Projects\GeneralSearchAlgo\Workbooks\HFM Master Data Frame for matching.xlsx"
+people_moves_file_path = r"C:\Users\Bay Street - Larry B\Documents\Brielle\Programming\Projects\GeneralSearchAlgo\Workbooks\People Moves Data Frame.xlsx"
+id_list_file_path = "K:\Market Maps\IDStorage\HFMUniqueIDs.txt"
 
 df_master = pd.read_excel(master_file_path, sheet_name='Master')
 df_people_moves = pd.read_excel(people_moves_file_path, sheet_name='People Moves')
+
+# Converted macro code
+def generate_unique_id(existingIDs):
+    maxNumber = 0000000
+    
+    for ID in existingIDs:
+        if len(ID) != 0:
+            numericComp = int(ID[3:])
+            if numericComp > maxNumber:
+                maxNumber = numericComp
+    
+    return ("HF_" + (maxNumber + 1))
+
+
+def read_ids_from_file(file_path):
+    file_series = pd.read_csv(file_path, header=None, squeeze=True)
+
+    array = file_series.to_numpy()
+    
+    return array
+    
+def write_id_to_file(file_path, id):
+    with open(file_path, 'a') as file:
+        file.write(id + '\n')
+
+
+def fill_blank_ids(file_path):
+    id_col = df_master['ID']
+    
+    # Read existing IDs from the file
+    existing_ids = read_ids_from_file(file_path)
+    
+    # Iterate over the column and fill in blank cells
+    for index, cell in id_col.items():
+        if pd.isna(cell):
+            new_id = generate_unique_id(existing_ids)
+            df_master.at[index, 'ID'] = new_id
+            
+            # Append the new ID to the list of existing IDs
+            existing_ids.append(new_id)
+            
+            # Write  new ID to the txt file
+            write_id_to_file(file_path, new_id)
+        
+
 
 # Function to split and get last name
 
@@ -54,7 +100,7 @@ threshold = 800
 
 # List to store the matched rows
 matched_rows = []
-
+"""
 # Perform fuzzy matching for each row in People Moves
 for index, row in df_people_moves.iterrows():
     if index >= 4376:
@@ -70,7 +116,10 @@ for index, row in df_people_moves.iterrows():
 
 # Create a DataFrame from the matched rows
 matched_df = pd.DataFrame(matched_rows)
+"""
 
 # Save the matched DataFrame to a new Excel workbook
-output_file_path = r"C:\Users\BSA-OliverJ'22\OneDrive\Desktop\OneDrive\Programming\Projects\GeneralizedSearchAlgo\Workbooks\MS to PPLMVS ID Match Algo Output Book.xlsx"
-matched_df.to_excel(output_file_path, index=False)
+output_file_path = r"C:\Users\Bay Street - Larry B\Documents\Brielle\Programming\Projects\GeneralSearchAlgo\Workbooks\MS to PPLMVS ID Match Algo Output Book.xlsx"
+"""matched_df.to_excel(output_file_path, index=False)
+"""
+fill_blank_ids(output_file_path)
